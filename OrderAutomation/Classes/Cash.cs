e68a6e7c388ceac7c8de91d9402b465644318942
EmreDemirci0 <76517477+EmreDemirci0@users.noreply.github.com>
2021-12-26 13:36:34 +0300

    Cash.cs

--- OrderAutomation/Classes/Cash.cs
+++ OrderAutomation/Classes/Cash.cs
@@ -6,10 +6,7 @@ namespace OrderAutomation
 {
     class Cash:Payment
     {
-       // public decimal cashTendered { get; set; }//ne kadarlık nakşit
-        //public override decimal amount { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        
-
+      
         public override void feePay(decimal price)
         {
             amount = price;
